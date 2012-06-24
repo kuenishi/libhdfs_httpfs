a729c3ef06ee123cf13f127cd3fc19d9ef39d405
UENISHI Kota <kuenishi+github@gmail.com>
2012-06-25 02:27:30 +0900

    mock

--- hdfs_httpfs.cpp
+++ hdfs_httpfs.cpp
@@ -1,4 +1,24 @@
+#include "hdfs_httpfs.hpp"
+
+namespace httpfs {
 
 int version(){
   return 0;
+}
+
+
+ifstream::ifstream(const char* filename)
+{
+}
+ifstream::~ifstream()
+{
+}
+
+ofstream::ofstream(const char* filename)
+{
+}
+ofstream::~ofstream()
+{
+}
+
 }
--- hdfs_httpfs.hpp
+++ hdfs_httpfs.hpp
@@ -1,3 +1,33 @@
 #pragma once
 
+#include <istream>
+#include <ostream>
+
+namespace httpfs {
+
 int version();
+
+class ifstream : public std::istream {
+public:
+  ifstream(const char*);
+  virtual ~ifstream();
+
+  template <typename T>
+  std::istream& operator >> (const T& t){
+    return this;
+  }
+};
+
+
+class ofstream : public std::ostream {
+  ofstream(const char*);
+  virtual ~ofstream();
+
+  template <typename T>
+  std::ostream& operator << (const T& t){
+    return this;
+  }
+
+};
+
+}
